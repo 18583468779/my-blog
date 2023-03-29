23e54b8ecbf0f740625cee110c56e5184982b92e
xiewen <1123654054@qq.com>
2023-03-29 13:25:39 +0800

    modify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-import Layout from "src/component/layout";
-
+import Layout from "src/component/Layout";
 import { store } from "src/redux/store";
 
 export default function App({ Component, pageProps }: AppProps) {
