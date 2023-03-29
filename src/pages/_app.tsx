792c68d796d4047d8bb3c3b4c235d5bd8504841e
xiewen <1123654054@qq.com>
2023-03-29 10:27:37 +0800

    删除 没有使用的文件api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
-  // console.log(pageProps);
   return (
     <Provider store={store}>
       <Layout>
