72f3f30f1d491aacd67b81b4de07ddd41b09444f
xiewen <1123654054@qq.com>
2023-03-30 10:03:58 +0800

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,11 @@
-echo Hello xiewen > /tmp/xie
+docker start dc84d6d1c295 &&
+cd /home/blog/app/ &&
+git pull &&
+npm install --production=false &&
+npm run build &&
+docker build -t xie/node-web-app . &&
+docker kill sleepy_davinci &&
+docker rm sleepy_davinci &&
+docker run --name sleepy_davinci --network=host -p 3000:3000 -d xie/node-web-app &&
+
+echo 'OK!'
